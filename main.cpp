bca666a32ea6cdcda7566c20fe9c4792dc71205e
l260732 <l260732@LAB13-05>
2026-09-08 09:02:16 +0500

    movign

--- main.cpp
+++ main.cpp
@@ -1,27 +1,50 @@
 #include<iostream>
-#include<conio.h>
 using namespace std;
 
 // Written by Muhammad Ali
-// 9/1/2026
+// Initially on 9/5/2026
+// Lab 03 - 9/8/2026
 // As there were issues with multiple files, this code exists to launch the needed one
+// https://github.com/Alimadcorp/C-at-FAST/blob/master/main.cpp
 
-int student();
-int calculator();
+int q1(); int q2(); int q3(); int q4(); int q5(); int q6();
+// predefined all functions for each task
 
 int main() {
-	start:
-	int target = 0;
-	cout << endl << "Enter task number (2-3): ";
-	cin >> target;
-	switch (target) {
-	case 2: student(); break;
-	case 3: calculator(); break;
-	}
+start:
+    system("clear");
+    int target = 0, r = 0; // r for result, if not 0, the program crashed..
+    cout << "Enter question number (1-6): ";
+    if (!(cin >> target)) { cin.clear(); cin.ignore(10000, '\n'); goto start; }
+run:
+    cout << endl;
+    switch (target) {
+    case 1: r = q1(); break;
+    case 2: r = q2(); break;
+    case 3: r = q3(); break;
+    case 4: r = q4(); break;
+    case 5: r = q5(); break;
+    case 6: r = q6(); break;
+    default: goto start;
+    }
 
-	cout << endl << "Press or g to select different task, press any other key to exit. ";
-	char o = _getche();
-	if (o == 'g' || o == 'G') goto start;
-	cout << "Quitting...";
-	return 0;
+    if (r == 0) {
+        cout << endl << "Enter g to select different question, n for next question, enter q to quit: ";
+    }
+    else {
+        cout << endl << "Program crashed with exit code of " << r << " press r to retry, g to select a different question, n for next question, enter q to quit: ";
+    }
+
+    char o;
+    if (!(cin >> o)) { cout << "\nWhy...\n\n"; return 0; }
+
+    switch (o) {
+    case 'n': case 'N': target++; if (target > 6) target = 1;
+    case 'r': case 'R': goto run;
+    case 'g': case 'G': goto start;
+    case 'q': case 'Q': cout << "\nQuitting...\n\n"; return 0;
+    default: cout << "\nQuitting anyways -_-\n\n";
+    }
+
+    return 0;
 }
