daf47e9047c4523e2a4531b6c4fde2b7b877d80c
l260732 <l260732@LAB13-05>
2026-09-08 11:03:37 +0500

    doeth

--- assignment1/l260732_q_no1.cpp
+++ assignment1/l260732_q_no1.cpp
@@ -2,7 +2,19 @@
 using namespace std;
 
 int q1() {
+    int p0, t, finalPopulation;
+    
     cout << "Question 1: The Bacterial Outbreak" << endl << endl;
+    cout << "Enter initial population: ";
+    cin >> p0;
+    cout << "Enter minutes taken per cycle: ";
+    cin >> t;
+
+    finalPopulation = p0 * 8;
+
+    cout << endl << "Time elapsed: " << (t * 3) / 60 << "h " << (t * 3) % 60;
+    cout << endl << "Final population: " << finalPopulation; // simply doubled thrice
+    cout << endl;
 
     return 0;
 }
--- assignment1/l260732_q_no2.cpp
+++ assignment1/l260732_q_no2.cpp
@@ -1,8 +1,19 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
 
 int q2() {
+    int withdrawlAmt;
+
     cout << "Question 2: The ATM Cash Dispenser" << endl << endl;
+    cout << "Enter amount to withdraw: ";
+    cin >> withdrawlAmt;
+
+    cout << endl << "Rs. 5000 notes: " << floor(withdrawlAmt / 5000.0) << endl;
+    cout << endl << "Rs. 1000 notes: " << floor((withdrawlAmt % 5000) / 1000.0) << endl;
+    cout << endl << "Rs.  500 notes: " << floor((withdrawlAmt % 1000) / 500.0) << endl;
+    cout << endl << "Rs.  100 notes: " << floor((withdrawlAmt % 500) / 100.0) << endl;
+    cout << endl << "Rs.   10 notes: " << floor((withdrawlAmt % 100) / 10.0) << endl;
 
     return 0;
 }
