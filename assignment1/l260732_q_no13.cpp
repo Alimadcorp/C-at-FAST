8e5983f73608b64a7ccb56f2746e8b4ed4b78c7b
Alimadcorp <119115454+Alimadcorp@users.noreply.github.com>
2026-09-10 00:04:07 +0500

    finish question 13

--- assignment1/l260732_q_no13.cpp
+++ assignment1/l260732_q_no13.cpp
@@ -2,7 +2,18 @@
 using namespace std;
 
 int q13() {
+    int age, clearance, overrideCode, access;
     cout << "Question 13. Logical Access Matrix" << endl << endl;
 
+    cout << "Enter age: ";
+    cin >> age;
+    cout << "Enter clearance: ";
+    cin >> clearance;
+    cout << "Enter override code: ";
+    cin >> overrideCode;
+
+    access = (age >= 18 && clearance > 3) || overrideCode == 999;
+
+    cout << endl << "Access granted: " << access << endl;
     return 0;
 }
