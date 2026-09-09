4b4bcfdb9071963c0f00846d2614e289c7bc4414
Alimadcorp <119115454+Alimadcorp@users.noreply.github.com>
2026-09-09 23:42:30 +0500

    finish question 9

--- assignment1/l260732_q_no9.cpp
+++ assignment1/l260732_q_no9.cpp
@@ -1,8 +1,24 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
 
 int q9() {
+    double P, r, n, t, A, interest;
     cout << "Question 9: Compound Interest Calculator" << endl << endl;
 
+    cout << "Enter principal amount: ";
+    cin >> P;
+    cout << "Enter annual interest rate: ";
+    cin >> r;
+    cout << "Enter compounds per year: ";
+    cin >> n;
+    cout << "Enter number of years: ";
+    cin >> t;
+
+    A = P * pow(1 + ((r / 100.0) / n), n * t);
+    interest = A - P;
+
+    cout << endl << "Final amount: " << A;
+    cout << endl << "Compond interest: " << interest << endl;
     return 0;
 }
