f9d337388abd6846a0c1020bda71d2d32a6710ee
Alimadcorp <119115454+Alimadcorp@users.noreply.github.com>
2026-09-10 00:22:50 +0500

    finish question 17 yayyy

--- assignment1/l260732_q_no17.cpp
+++ assignment1/l260732_q_no17.cpp
@@ -2,7 +2,37 @@
 using namespace std;
 
 int q17() {
-    cout << "Quesiton 17. 3D Distance Calculator" << endl << endl;
+    double x1, y1, z1, x2, y2, z2, sqDist, dist;
 
+    cout << "Quesiton 17. 3D Distance Calculator" << endl << endl;
+    cout << "Enter data for first point:" << endl << "X: ";
+    cin >> x1;
+    cout << "Y: ";
+    cin >> y1;
+    cout << "Z: ";
+    cin >> z1;
+    cout << endl << "Enter data for second point:" << endl << "X: ";
+    cin >> x2;
+    cout << "Y: ";
+    cin >> y2;
+    cout << "Z: ";
+    cin >> z2;
+
+    sqDist = (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2 - z1) * (z2 - z1);
+
+    dist = sqDist / 2; // letes compute the square root of it P:
+    dist = (dist + sqDist / dist) / 2.0;
+    dist = (dist + sqDist / dist) / 2.0;
+    dist = (dist + sqDist / dist) / 2.0;
+    dist = (dist + sqDist / dist) / 2.0;
+    dist = (dist + sqDist / dist) / 2.0;
+    dist = (dist + sqDist / dist) / 2.0;
+    dist = (dist + sqDist / dist) / 2.0;
+    dist = (dist + sqDist / dist) / 2.0;
+    dist = (dist + sqDist / dist) / 2.0;
+    dist = (dist + sqDist / dist) / 2.0;
+
+    cout << endl << "Squared distance: " << sqDist << endl;
+    cout << "Distance: " << dist << endl;
     return 0;
 }
