405f343dc50cdd7d6a9c1ad624a47390a9ef7826
Alimadcorp <119115454+Alimadcorp@users.noreply.github.com>
2026-09-09 23:49:22 +0500

    finish question 10

--- assignment1/l260732_q_no10.cpp
+++ assignment1/l260732_q_no10.cpp
@@ -2,7 +2,15 @@
 using namespace std;
 
 int q10() {
+    int chirpsPerMinute; //  i wonder who would count this :sob:
+    double temperature;
     cout << "Question 10: Cricket Chirp Temperature Calculator" << endl << endl;
 
+    cout << "Enter chirps per minute: ";
+    cin >> chirpsPerMinute;
+
+    temperature = 50 + (chirpsPerMinute - 40) / 4.0;
+
+    cout << endl << "Temprature (F): " << temperature << endl;
     return 0;
 }
