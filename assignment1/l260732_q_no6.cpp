c7ec5263b04a55e7778c05bcac704350c932b53e
Alimadcorp <119115454+Alimadcorp@users.noreply.github.com>
2026-09-09 23:04:43 +0500

    do question 6

--- assignment1/l260732_q_no6.cpp
+++ assignment1/l260732_q_no6.cpp
@@ -2,7 +2,19 @@
 using namespace std;
 
 int q6() {
+    int totalSeconds;
+    int hours, minutes, seconds;
+
     cout << "Question 6: Time Conversion" << endl << endl;
+    cout << "Enter total amount of seconds: ";
+    cin >> totalSeconds;
+
+    hours = totalSeconds / 3600;
+    minutes = (totalSeconds % 3600) / 60;
+    seconds = totalSeconds % 60;
+
+    cout << endl << hours << " hours, " << minutes << " minutes, " << seconds << " seconds";
+    cout << endl;
 
     return 0;
 }
