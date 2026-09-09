bc6cbb31bdded4c46fc1f7594625c5e764ea1aa8
Alimadcorp <119115454+Alimadcorp@users.noreply.github.com>
2026-09-09 23:21:51 +0500

    finish question 7

--- assignment1/l260732_q_no7.cpp
+++ assignment1/l260732_q_no7.cpp
@@ -2,7 +2,25 @@
 using namespace std;
 
 int q7() {
-    cout << "Question 7: Three-Digit Number Analysis" << endl << endl;
+    int num, sum, reverse;
+    int d1, d2, d3; // digiets 1 to four, d1 is thousands place
 
+    cout << "Question 7: Three-Digit Number Analysis" << endl << endl;
+    cout << "Enter a number: ";
+    cin >> num;
+    
+    d1 = num / 100;
+    d2 = (num % 100) / 10;
+    d3 = num % 10;
+
+    sum = d1 + d2 + d3;
+    reverse = d3 * 100 + d2 * 10 + d1;
+    
+    cout << endl << "Hundreds digit: " << d1;
+    cout << endl << "Tens digit: " << d2;
+    cout << endl << "Ones digit: " << d3;
+    cout << endl << "Sum of digits: " << sum;
+    cout << endl << "Digits in reverse: " << reverse;
+    cout << endl;
     return 0;
 }
