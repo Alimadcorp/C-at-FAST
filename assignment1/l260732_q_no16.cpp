ea2c43b43219899083bbcca76e54b6ae4d5aa24f
Alimadcorp <119115454+Alimadcorp@users.noreply.github.com>
2026-09-10 00:13:42 +0500

    finish question 16

--- assignment1/l260732_q_no16.cpp
+++ assignment1/l260732_q_no16.cpp
@@ -2,7 +2,17 @@
 using namespace std;
 
 int q16() {
-    cout << "Question 16. Digital Clock Converter" << endl << endl;
+    int seconds, hrs, min, sec;
 
+    cout << "Question 16. Digital Clock Converter" << endl << endl;
+    cout << "Enter seconds: ";
+    cin >> seconds;
+
+    hrs = seconds / 3600;
+    sec = seconds % 3600;
+    min = sec / 60;
+    sec = sec % 60;
+
+    cout << endl << "Hours: " << hrs << " Minutes: " << min << " Seconds: " << sec << endl;
     return 0;
 }
