2b9455725517839bd135bdfd6db1cb8db315a7f1
Alimadcorp <119115454+Alimadcorp@users.noreply.github.com>
2026-09-10 00:01:38 +0500

    finish question 12

--- assignment1/l260732_q_no12.cpp
+++ assignment1/l260732_q_no12.cpp
@@ -5,5 +5,13 @@ int q12() {
     double mass, velocity, kineticEnergy;
     cout << "Question 12. The Kinetic Energy Trap" << endl << endl;
 
+    cout << "Enter mass: ";
+    cin >> mass;
+    cout << "Enter velocity: ";
+    cin >> velocity;
+
+    kineticEnergy = static_cast<double>(1)/2 * mass * velocity * velocity;
+
+    cout << endl << "Kinetic energy: " << kineticEnergy << endl;
     return 0;
 }
