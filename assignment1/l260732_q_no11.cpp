5103be8ad5981bb5b148b876c89a28a5812ffb81
Alimadcorp <119115454+Alimadcorp@users.noreply.github.com>
2026-09-09 23:58:07 +0500

    finish question 11

--- assignment1/l260732_q_no11.cpp
+++ assignment1/l260732_q_no11.cpp
@@ -2,7 +2,19 @@
 using namespace std;
 
 int q11() {
+    int math, physics, programming;
+    double average;
     cout << "Question 11: Student Average Calculator" << endl << endl;
 
+    cout << "Enter obtained marks in math: ";
+    cin >> math;
+    cout << "Enter obtained marks in physics: ";
+    cin >> physics;
+    cout << "Enter obtained marks in programming: ";
+    cin >> programming;
+
+    average = (math + physics + programming) / 3.0;
+
+    cout << endl << "Average: " << average << endl;
     return 0;
 }
