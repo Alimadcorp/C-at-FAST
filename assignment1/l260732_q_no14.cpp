230a5ec7ba4d01dc5efd2dac2387b85eb50445cb
Alimadcorp <119115454+Alimadcorp@users.noreply.github.com>
2026-09-10 00:07:02 +0500

    finish question 14

--- assignment1/l260732_q_no14.cpp
+++ assignment1/l260732_q_no14.cpp
@@ -2,7 +2,18 @@
 using namespace std;
 
 int q14() {
+    int fileSize, packetSize, packets, remaining;
     cout << "Question 14. Network Packet Fragmentation" << endl << endl;
+    
+    cout << "Enter message size: ";
+    cin >> fileSize;
+    cout << "Enter packet size: ";
+    cin >> packetSize;
 
+    packets = fileSize / packetSize;
+    remaining = fileSize % packetSize;
+
+    cout << endl << "Total packets: " << packets;
+    cout << endl << "Leftover: " << remaining << endl;
     return 0;
 }
